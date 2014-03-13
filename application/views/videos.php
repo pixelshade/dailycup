daab00985d8e4498e750ad3bf03af2eab430a147
pixelshade <psuldo@gmail.com>
2014-03-13 13:18:54 +0100

    again text update + image

--- application/views/videos.php
+++ application/views/videos.php
@@ -82,8 +82,13 @@ if($user_profile != NULL){
 	?>
 
 	<div class="jumbotron">
-		<h1>Your daily cup of..</h1>
-		<p class="lead">You tell us! O co sa jedna? Ak vas napadlo nieco o co by ste sa chceli podelit, tak natocte video. </p>
+		<h1 class="hidden">Your daily cup of..</h1>
+		<p>
+		<img src="/assets/img/CoffeeArmy-banner3.jpg" alt="banner" width="100%" />
+		</p>
+		<p class="lead"><h3>Whatevr!</h3> Natočte krátke video, alebo si pozrite ostatné. </p>
+		<p><h3>O čo sa jedná? </h3>
+		Kedže sa už všetci tak často nestretávame, tak vzniká tento virtuálny denník, kde možme rageovať, tešiť sa, vzdelavať alebo len povedať čo ste jedli na raňajky a že vám chutí.</p>
 		<p><a class="btn btn-lg btn-success" href="<?php echo $login_url; ?>" role="button">Login via Fb</a></p>
 
 	</div>
